2635d26aee37065b3be2fbe2785103b029fd367a
Shahriyar001 <shahriyarmahbub55@gmail.com>
2022-11-28 17:54:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dawimtn.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const productCollection = client.db('resaleMarket').collection('products');
+        const bookingsCollection = client.db('resaleMarket').collection('bookings');
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const options = await productCollection.find(query).toArray();
+            res.send(options);
+
+        })
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body
+            console.log(booking);
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
+
+app.get('/', async (req, res) => {
+    res.send('running')
+})
+
+app.listen(port, () => console.log(`port runing on ${port}`))
